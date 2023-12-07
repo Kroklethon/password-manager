9d39af899a23a9016e6b96cb56e98822a9dd627c
evan lhuissier <evanlhuissier73@gmail.com>
2023-12-07 11:11:27 +0100

    decrypt working

--- ProjetC#/LoginWindow.cs
+++ ProjetC#/LoginWindow.cs
@@ -29,44 +29,71 @@ namespace ProjetC_
                     filePath = openFileDialog.FileName;
 
                     label1.Text = filePath;
+                }
+            }
+        }
 
-                    //Read the contents of the file into a stream
-                    var fileStream = openFileDialog.OpenFile();
-
-                    using (StreamReader reader = new StreamReader(fileStream))
+        private void decryptFile(string hashedPassword)
+        {
+            using (System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create())
+            {
+                //Get half of the hashed password to create the key
+                aes.Key = Encoding.UTF8.GetBytes(hashedPassword.Substring(0, hashedPassword.Length / 2));
+                //Get quarter of the hashed password to create the initial vector
+                aes.IV = Encoding.UTF8.GetBytes(hashedPassword.Substring(0, hashedPassword.Length / 4));
+                XmlSerializer serializer = new XmlSerializer(typeof(List<PasswordEntry>));
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(fileStream, aes.CreateDecryptor(), CryptoStreamMode.Read))
                     {
-                        XmlSerializer serializer = new XmlSerializer(typeof(List<PasswordEntry>));
-                        passwordEntries = (List<PasswordEntry>)serializer.Deserialize(reader);
+                        //we raise an exception if the password is incorrect
+                        try
+                        {
+                            passwordEntries = (List<PasswordEntry>)serializer.Deserialize(cryptoStream);
+                        }
+                        catch (Exception)
+                        {
+                            throw new Exception("Mot de passe incorrect");
+                        }   
                     }
                 }
             }
         }
+
+        private bool checkPassword(string enteredPassword)
+        {
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                byte[] bytes = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(enteredPassword));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                string hashedPassword = builder.ToString();
+                try
+                {
+                    decryptFile(hashedPassword);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
         private void btnLogin_Click(object sender, EventArgs e)
         {
             string enteredPassword = txtbxPassword.Text;
             string hashedPassword = string.Empty;
-            string initialVector = string.Empty;
-            if (enteredPassword == masterPassword)
+            if (checkPassword(enteredPassword))
             {
-                using (SHA256 mySHA256 = SHA256.Create())
-                {
-                    byte[] bytes = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(enteredPassword));
-                    StringBuilder builder = new StringBuilder();
-                    for (int i = 0; i < bytes.Length; i++)
-                    {
-                        builder.Append(bytes[i].ToString("x2"));
-                    }
-                    hashedPassword = builder.ToString();
-                    Debug.WriteLine(hashedPassword);
-
-                    MainWindow mainWindow = new MainWindow();
-                    mainWindow.SetPasswordEntries(passwordEntries);
-                    mainWindow.HashedPassword = hashedPassword;
-                    mainWindow.Show();
-                    this.Hide();
-                    mainWindow.FormClosed += (s, args) => this.Show();
-                }
-
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.SetPasswordEntries(passwordEntries);
+                mainWindow.HashedPassword = hashedPassword;
+                mainWindow.Show();
+                this.Hide();
             }
             else
             {
